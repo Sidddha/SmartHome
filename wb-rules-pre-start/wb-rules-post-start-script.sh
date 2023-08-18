a1ec71060b5289007b7383aa9985f1c5fabd6148
Sidddha <78275957+Sidddha@users.noreply.github.com>
2023-08-18 19:35:06 +0300

    Update wb-rules-post-start-script.sh

--- wb-rules-pre-start/wb-rules-post-start-script.sh
+++ wb-rules-pre-start/wb-rules-post-start-script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 sleep 30
-echo '{"restrict_buttons": false }' > '/etc/wb-rules-pre-start/on-start-restrict.json' 
+echo '{"restrict_buttons": false }' > '/etc/wb-rules/on-start-restrict.json' 
